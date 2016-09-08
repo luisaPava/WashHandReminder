dd3b57a5093a45f9b30aaccd2842545cfbc50484
Luisa Mello <lu_mello93@hotmail.com>
2016-09-08 14:44:17 -0300

    Contraint, scroll e novas telas

---
+++ handWashReminder/handWashReminder/InstrucoesViewController.swift
@@ -0,0 +1,43 @@
+//
+//  InstrucoesViewController.swift
+//  handWashReminder
+//
+//  Created by Luisa Mello on 9/8/16.
+//  Copyright © 2016 Bepid. All rights reserved.
+//
+
+import UIKit
+
+class InstrucoesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
+        self.navigationController?.navigationBar.barTintColor = UIColor(netHex: 0x2F557D)
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
